846a224e21d99dd008f8298c8f63baeea10c219a
priyamvada <130129372+Vadapaav1@users.noreply.github.com>
2024-08-03 21:44:45 +0530

    Add files via upload

---
+++ contact us/scripts.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const form = document.querySelector('.contact-form');
+    
+//     form.addEventListener('submit', (event) => {
+//         event.preventDefault();
+//         // Handle form submission
+//         alert('Form submitted successfully!');
+//     });
+// });
